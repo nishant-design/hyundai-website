3efd26266a0481d8905ad57eb5102fdc454a7735
nishant personal <nishant.chgh@gmail.com>
2023-01-15 13:06:21 +0530

    changes

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div>
-      <h1>This is Homepage</h1>
+      <h1 style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '98%', textAlign: 'center'}}>This is Hyundai Homepage</h1>
     </div>
   )
 }
