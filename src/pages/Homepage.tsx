2929e9b850542d85b752673a5d78fbbae7223cc3
nishant personal <nishant.chgh@gmail.com>
2023-03-03 12:00:13 +0530

    implemented slick slider

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import FixedFooter from "../components/Homepage/fixedFooter/FixedFooter";
 import HeroSlider from "../components/Homepage/heroSlider/HeroSlider";
+import HomepageSlider from "../components/Homepage/heroSlider/HomepageSlider";
 
 const Homepage = () => {
   return (
     <>
       <HeroSlider />
+      <HomepageSlider/>
       <FixedFooter/>
     </>
   )
