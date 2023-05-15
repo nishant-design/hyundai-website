534ac07762df46139598583ea62e31c32600d8b4
nishant personal <nishant.chgh@gmail.com>
2023-05-15 16:19:56 +0530

    admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+
+const Admin = () => {
+  return(
+    <>
+      <Typography variant="h3">ADMIN FORM COMES HERE</Typography>
+    </>
+  )
+}
+
+export default Admin;
